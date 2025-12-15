263af05fc2a3d31afbbc175caad1d98311fdbdda
jromero5733 <jromero5733@utm.edu.ec>
2025-12-15 15:01:28 -0500

    Actulizacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     //const API_URL = "https://backend-express-ci-cd.onrender.com";
 
     //fetch("http://localhost:4000/api/saludo")
-    fetch("https://backend-express-ci-cd-latest-x0ae.onrender.com/" + "api/saludo")
+    fetch("https://backend-express-ci-cd-latest-zwrn.onrender.com/" + "api/saludo")
       .then((res) => res.json())
       .then((data) => setMensaje(data.mensaje))
       .catch(() =>
